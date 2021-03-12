b0b28637a0f6481b1e2a10b5ccce65ad8df813cb
rahu2000 <rahu2000@naver.com>
2021-03-12 16:45:36 +0900

    add fsx storage class creator

--- fsx-csi/fsx_storageclass_creator.sh
+++ fsx-csi/fsx_storageclass_creator.sh
@@ -8,6 +8,33 @@
 #
 # Tested version
 #   EKS v1.19
-#   aws-fsx-csi 0.4.0
 ##############################################################
 #!/bin/bash
+
+export FSX_SC_NAME="fsx-private-a" # example: prefix-[public|private]-az
+export SUBNET_ID="subnet-05df3c1294978641b"
+export SECURITY_GROUP_ID="sg-021bf003b3c82d636"
+export S3_BUCKET_NAME="fsx-$(uuidgen | awk '{print tolower($0)}')"
+export SCRACH_TYPE="SCRATCH_2"
+
+if [[ -z $(aws s3 ls 2>/dev/null | grep $S3_BUCKET_NAME) ]]; then
+  aws s3 mb s3://$S3_BUCKET_NAME
+fi
+
+LOCAL_OS_KERNEL="$(uname -a | awk -F ' ' ' {print $1} ')"
+
+if [ "Darwin" == "$LOCAL_OS_KERNEL" ]; then
+  sed -i.bak "s|FSX_SC_NAME|${FSX_SC_NAME}|g" ./templates/fsx-storageclass.yaml
+  sed -i '' "s|SUBNET_ID|${SUBNET_ID}|g" ./templates/fsx-storageclass.yaml
+  sed -i '' "s|SECURITY_GROUP_ID|${SECURITY_GROUP_ID}|g" ./templates/fsx-storageclass.yaml
+  sed -i '' "s|S3_BUCKET_NAME|${S3_BUCKET_NAME}|g" ./templates/fsx-storageclass.yaml
+  sed -i '' "s|SCRACH_TYPE|${SCRACH_TYPE}|g" ./templates/fsx-storageclass.yaml
+else
+  sed -i.bak "s|FSX_SC_NAME|${FSX_SC_NAME}|g" ./templates/fsx-storageclass.yaml
+  sed -i '' "s/SUBNET_ID/${SUBNET_ID}/g" ./templates/fsx-storageclass.yaml
+  sed -i '' "s/SECURITY_GROUP_ID/${SECURITY_GROUP_ID}/g" ./templates/fsx-storageclass.yaml
+  sed -i '' "s/S3_BUCKET_NAME/${S3_BUCKET_NAME}/g" ./templates/fsx-storageclass.yaml
+  sed -i '' "s/SCRACH_TYPE/${SCRACH_TYPE}/g" ./templates/fsx-storageclass.yaml
+fi
+
+kubectl apply -f ./templates/fsx-storageclass.yaml
