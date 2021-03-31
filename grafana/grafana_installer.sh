bdb50031e6b3b2bbe673e100117d0c3637fbaab3
Rahu2000 <rahu2000@naver.com>
2021-03-31 23:46:49 +0900

    grafana init

---
+++ grafana/grafana_installer.sh
@@ -0,0 +1,15 @@
+##############################################################
+# GRAFANA INSTALLER
+#
+# Required tools
+# - helm v3+
+# - jq 1.6+
+# - kubectl 1.16+
+#
+# Tested version
+#   EKS v1.19
+#   chart: bitnami/grafana-operator (0.6.4)
+##############################################################
+#!/bin/bash
+
+set -x
