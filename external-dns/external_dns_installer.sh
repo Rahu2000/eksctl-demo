06d6a0282c4ef8e9cd9ec910b3ceff9e8abcb99d
Rahu2000 <rahu2000@naver.com>
2021-06-11 17:24:11 +0900

    initial external-dns-installer

--- external-dns/external_dns_installer.sh
+++ external-dns/external_dns_installer.sh
@@ -18,8 +18,17 @@ export NAMESPACE="external-dns"
 export SERVICE_ACCOUNT="external-dns"
 export CHART_VERSION="5.0.3"
 export REGION="ap-northeast-2"
+export ZONETYPE="public"
+export DOMAINS="eksdemo.tk"
+export RELESE_NAME="external-dns-${ZONETYPE}"
 
 source ../common/utils.sh
+
+HOSTZONEID="$(aws route53 list-hosted-zones-by-name --output json --dns-name "$DOMAINS." | jq -r '.HostedZones[0].Id' | awk -F '/' '{print $3}')"
+if [ -z $HOSTZONEID ]; then
+  echo "HostedZone is not found."
+  exit 0
+fi
 
 ##############################################################
 # Create IAM Role and ServiceAccount
@@ -45,9 +54,22 @@ helm repo update
 
 if [ "Darwin" == "$LOCAL_OS_KERNEL" ]; then
   sed -i.bak "s|IAM_ROLE_ARN|${IAM_ROLE_ARN}|g" ./templates/external-dns-values.yaml
-  sed -i '' "s|SERVICE_ACCOUNT|${SERVICE_ACCOUNT}|g" ./templates/external-dns-values.yaml
+  sed -i '' "s|REGION|${REGION}|g" ./templates/external-dns-values.yaml
+  sed -i '' "s|ZONETYPE|${ZONETYPE}|g" ./templates/external-dns-values.yaml
+  sed -i '' "s|DOMAINS|${DOMAINS}|g" ./templates/external-dns-values.yaml
+  sed -i '' "s|HOSTZONEID|${HOSTZONEID}|g" ./templates/external-dns-values.yaml
 else
   IAM_ROLE_ARN=$(echo ${IAM_ROLE_ARN} | sed 's|\/|\\/|')
   sed -i.bak "s/IAM_ROLE_ARN/${IAM_ROLE_ARN}/g" ./templates/external-dns-values.yaml
-  sed -i "s/SERVICE_ACCOUNT/${SERVICE_ACCOUNT}/g" ./templates/external-dns-values.yaml
+  sed -i "s/REGION/${REGION}/g" ./templates/external-dns-values.yaml
+  sed -i '' "s|ZONETYPE|${ZONETYPE}|g" ./templates/external-dns-values.yaml
+  sed -i '' "s|DOMAINS|${DOMAINS}|g" ./templates/external-dns-values.yaml
+  sed -i '' "s|HOSTZONEID|${HOSTZONEID}|g" ./templates/external-dns-values.yaml
 fi
+
+helm upgrade --install ${RELESE_NAME} \
+  bitnami/external-dns \
+  --version=${CHART_VERSION} \
+  --namespace ${NAMESPACE} \
+  -f ./templates/external-dns-values.yaml \
+  --wait
