82ad9c1177de6aa27ffdabfd23bb184752336cae
rahu2000 <rahu2000@naver.com>
2021-08-18 16:44:38 +0900

    bump up efs csi version

--- efs-csi/efs_csi_installer.sh
+++ efs-csi/efs_csi_installer.sh
@@ -7,8 +7,10 @@
 # - kubectl 1.16+
 #
 # Tested version
-#   EKS v1.19
+# - EKS v1.19
 #   chart: aws-efs-csi 1.1.2 (1.1.1)
+# - EKS v1.21
+#   chart: aws-efs-csi 1.2.4 (1.2.1)
 ##############################################################
 #!/bin/bash
 
@@ -17,14 +19,18 @@ export IAM_POLICY_NAME="AmazonEKS_EFS_CSI_Driver_Policy"
 export IAM_ROLE_NAME="AmazonEFS_EFS_CSI_Driver_Role"
 export SERVICE_ACCOUNT="efs-csi-controller"
 export NAMESPACE="kube-system"
-export CHART_VERSION="v1.1.2"
+export CHART_VERSION="v1.2.4"
 export REGION="ap-northeast-2"
-export CREATE_CONTROLLER="false" # [true|false]
+export CREATE_CONTROLLER="true" # [true|false]
 
 source ../common/utils.sh
 
 IAM_ROLE_ARN=""
 IAM_POLICY_ARN=""
+
+DIR=(`date "+%Y-%m-%d-%H%M%S"`)
+mkdir -p /tmp/$DIR
+cp ./templates/*.yaml /tmp/${DIR}/
 
 if [[ "true" == $CREATE_CONTROLLER ]]; then
   ##############################################################
@@ -56,17 +62,17 @@ fi
 helm repo update
 
 if [ "Darwin" == "$LOCAL_OS_KERNEL" ]; then
-  sed -i.bak "s|CREATE_CONTROLLER|${CREATE_CONTROLLER}|g" ./templates/efs-csi-driver.values.yaml
+  sed -i.bak "s|CREATE_CONTROLLER|${CREATE_CONTROLLER}|g" /tmp/${DIR}/efs-csi-driver.values.yaml
   if [[ "true" == $CREATE_CONTROLLER ]]; then
-    sed -i '' "s|SERVICE_ACCOUNT|${SERVICE_ACCOUNT}|g" ./templates/efs-csi-driver.values.yaml
-    sed -i '' "s|IAM_ROLE_ARN|${IAM_ROLE_ARN}|g" ./templates/efs-csi-driver.values.yaml
+    sed -i '' "s|SERVICE_ACCOUNT|${SERVICE_ACCOUNT}|g" /tmp/${DIR}/efs-csi-driver.values.yaml
+    sed -i '' "s|IAM_ROLE_ARN|${IAM_ROLE_ARN}|g" /tmp/${DIR}/efs-csi-driver.values.yaml
   fi
 else
-  sed -i.bak "s/CREATE_CONTROLLER/${CREATE_CONTROLLER}/g" ./templates/efs-csi-driver.values.yaml
+  sed -i.bak "s/CREATE_CONTROLLER/${CREATE_CONTROLLER}/g" /tmp/${DIR}/efs-csi-driver.values.yaml
   if [[ "true" == $CREATE_CONTROLLER ]]; then
     IAM_ROLE_ARN=$(echo ${IAM_ROLE_ARN} | sed 's|\/|\\/|')
-    sed -i "s/SERVICE_ACCOUNT/${SERVICE_ACCOUNT}/g" ./templates/efs-csi-driver.values.yaml
-    sed -i "s/IAM_ROLE_ARN/${IAM_ROLE_ARN}/g" ./templates/efs-csi-driver.values.yaml
+    sed -i "s/SERVICE_ACCOUNT/${SERVICE_ACCOUNT}/g" /tmp/${DIR}/efs-csi-driver.values.yaml
+    sed -i "s/IAM_ROLE_ARN/${IAM_ROLE_ARN}/g" /tmp/${DIR}/efs-csi-driver.values.yaml
   fi
 fi
 
@@ -74,12 +80,12 @@ helm upgrade --install aws-efs-csi-driver \
   aws-efs-csi-driver/aws-efs-csi-driver \
   --version=${CHART_VERSION} \
   --namespace ${NAMESPACE} \
-  -f ./templates/efs-csi-driver.values.yaml \
+  -f /tmp/${DIR}/efs-csi-driver.values.yaml \
   --wait
 
 if [[ "true" == $CREATE_CONTROLLER ]]; then
   ##############################################################
   ## Create a PodDisruptionBudget
   ##############################################################
-  kubectl apply -f ./templates/efs-csi-controller-pdb.yaml
+  kubectl apply -f /tmp/${DIR}/efs-csi-controller-pdb.yaml
 fi
