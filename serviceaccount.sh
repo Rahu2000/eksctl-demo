e4346b31a793d3050cefdf0bc3984cb5b05c81ce
Rahu2000 <rahu2000@naver.com>
2021-11-29 15:09:23 +0900

    add serviceaccount.sh

---
+++ serviceaccount.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export SERVICEACCOUNT=admin
+export NAMESPACE=admin
+
+CONTEXT=$(kubectl config current-context)
+kubectl create na ${NAMESPACE}
+kubectl create sa ${SERVICEACCOUNT} -n ${NAMESPACE}
+kubectl create clusterrolebinding ${SERVICEACCOUNT}-cluster-admin-rolebinding \
+    --clusterrole=cluster-admin \
+    --serviceaccount=${NAMESPACE}:{SERVICEACCOUNT} \
+    --namespace=${NAMESPACE}
+
+TOKEN=$(kubectl get secret --context ${CONTEXT} \
+    $(kubectl get serviceaccount ${SERVICEACCOUNT} \
+       --context ${CONTEXT} \
+       -n ${NAMESPACE} \
+       -o jsonpath='{.secrets[0].name}') \
+    -n ${NAMESPACE} \
+    -o jsonpath='{.data.token}' | base64 --decode)
+
+kubectl config set-credentials ${SERVICEACCOUNT}-token-user --token ${TOKEN}
+kubectl config set-context $CONTEXT --user ${SERVICEACCOUNT}-token-user
+
+# delete exists cluster/context/user
+# kubectl config delete-cluster
+# kubectl config delete-context
+# kubectl config delete-user
