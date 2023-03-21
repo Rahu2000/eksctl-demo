4b4c308826869d6b5e751df5bf6d1d7b206c230f
TaeHyeong Kim <rahu2000@naver.com>
2023-03-21 06:54:37 +0000

    modify: get token

--- serviceaccount.sh
+++ serviceaccount.sh
@@ -1,24 +1,59 @@
 #!/bin/bash
 
-export SERVICEACCOUNT="admin"
-export NAMESPACE="admin"
+read -p "Enter Service Account name [admin]: " sa
+SERVICEACCOUNT=${sa:-admin}
+
+read -p "Enter Service Account's Namespace [admin]: " ns
+NAMESPACE=${ns:-admin}
 
 CONTEXT="$(kubectl config current-context)"
 
-kubectl create ns "${NAMESPACE}"
-kubectl create sa "${SERVICEACCOUNT}" -n "${NAMESPACE}"
-kubectl create clusterrolebinding "${SERVICEACCOUNT}-cluster-admin-rolebinding" \
-    --clusterrole="cluster-admin" \
-    --serviceaccount="${NAMESPACE}:${SERVICEACCOUNT}" \
-    --namespace="${NAMESPACE}"
+# Create namespace for service account
+if [ -z "$(kubectl get ns | grep "$NAMESPACE")" ]; then
+  kubectl create ns "${NAMESPACE}"
+fi
 
+# Create service account
+if [ -z "$(kubectl get sa -n "${NAMESPACE}" | grep "$SERVICEACCOUNT")" ]; then
+  kubectl create sa "${SERVICEACCOUNT}" -n "${NAMESPACE}"
+fi
+
+# Attach the cluster admin role to the service account
+if [ -z "$(kubectl get clusterrolebinding | grep "${SERVICEACCOUNT}-cluster-admin-rolebinding")" ]; then
+  kubectl create clusterrolebinding "${SERVICEACCOUNT}-cluster-admin-rolebinding" \
+    --clusterrole="cluster-admin" \
+    --serviceaccount="${NAMESPACE}:${SERVICEACCOUNT}"
+fi
+
+#TOKEN=$(kubectl get secret --context "${CONTEXT}" \
+#    $(kubectl get serviceaccount "${SERVICEACCOUNT}" \
+#       --context "${CONTEXT}" \
+#       -n "${NAMESPACE}" \
+#       -o jsonpath='{.secrets[0].name}') \
+#    -n "${NAMESPACE}" \
+#-o jsonpath='{.data.token}' | base64 --decode)
+
+# Create long-lived API Token
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${SERVICEACCOUNT}-secret
+  namespace: ${NAMESPACE}
+  annotations:
+    kubernetes.io/service-account.name: ${SERVICEACCOUNT}
+type: kubernetes.io/service-account-token
+EOF
+
+
+# Get token
 TOKEN=$(kubectl get secret --context "${CONTEXT}" \
     $(kubectl get serviceaccount "${SERVICEACCOUNT}" \
        --context "${CONTEXT}" \
        -n "${NAMESPACE}" \
        -o jsonpath='{.secrets[0].name}') \
     -n "${NAMESPACE}" \
-    -o jsonpath='{.data.token}' | base64 --decode)
+-o jsonpath='{.items[*].data.token}' | base64 --decode)
 
 kubectl config set-credentials "${SERVICEACCOUNT}-token-user" --token "${TOKEN}"
 kubectl config set-context "${CONTEXT}" --user "${SERVICEACCOUNT}-token-user"
